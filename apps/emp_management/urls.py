466d206199b4d74284608ba9499ac38dca52c5e4
Senesh Akmeemana <seneshak2001@gmail.com>
2024-12-18 09:19:20 +0530

    Added new project named employee management system.

---
+++ apps/emp_management/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # path("", views.employee_list, name='employee_list'),
+    # path("add/", views.add_employee, name='add_employee'),
+    # path("<int:pk>/", views.employee_details, name='employee_details'),
+]
