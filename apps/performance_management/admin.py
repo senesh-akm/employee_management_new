7249d520684d09c727c189632e61a7be92357b11
Senesh Akmeemana <seneshak2001@gmail.com>
2024-12-24 20:51:31 +0530

    refactor: Created admin interfaces for performance management.

--- apps/performance_management/admin.py
+++ apps/performance_management/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from .models import PerformanceReview, GoalSetting, FeedbackMechanism
 
-# Register your models here.
+@admin.register(PerformanceReview)
+class PerformanceReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        "employee", 
+        "review_text", 
+        "appraisal_text", 
+    )
+    search_fields = ("employee__full_name", "review_text")
+    list_filter = ("employee",)
+    ordering = ("-employee",)
+    fieldsets = (
+        ("Employee Information", {"fields": ["employee"]}),
+        ("Review Details", {"fields": ["review_text", "appraisal_text"]}),
+    )
+
+@admin.register(GoalSetting)
+class GoalSettingAdmin(admin.ModelAdmin):
+    list_display = (
+        "employee", 
+        "goals", 
+    )
+    search_fields = ("employee__full_name", "goals")
+    list_filter = ("employee",)
+    ordering = ("-employee",)
+    fieldsets = (
+        ("Employee Information", {"fields": ["employee"]}),
+        ("Goals", {"fields": ["goals"]}),
+    )
+
+@admin.register(FeedbackMechanism)
+class FeedbackMechanismAdmin(admin.ModelAdmin):
+    list_display = (
+        "employee", 
+        "feedback", 
+    )
+    search_fields = ("employee__full_name", "feedback")
+    list_filter = ("employee",)
+    ordering = ("-employee",)
+    fieldsets = (
+        ("Employee Information", {"fields": ["employee"]}),
+        ("Feedback Details", {"fields": ["feedback"]}),
+    )
